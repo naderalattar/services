430844cdf9132d987de2f5102a53c84e08f39345
nader <naderalattar8eng@gmail.com>
2023-01-28 04:03:58 +0200

    adding main classes

---
+++ userAdimnstration/src/test/java/com/userAdimnstration/userAdimnstration/UserAdimnstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userAdimnstration.userAdimnstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdimnstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
